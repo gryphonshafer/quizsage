252bd64d2393d595f9b9198036204bc2e9b87667
Gryphon Shafer <gryphon@goldenguru.com>
2024-01-12 16:03:15 -0800

    Dest DB verify OCD

--- config/db/app/1688151748_quiz_season/verify.sql
+++ config/db/app/1688151748_quiz_season/verify.sql
@@ -1,6 +1,8 @@
 SELECT CASE WHEN
-    ( SELECT 1 FROM sqlite_master WHERE type = 'table'   AND name = 'season' ) +
-    ( SELECT 1 FROM sqlite_master WHERE type = 'table'   AND name = 'meet'   ) +
-    ( SELECT 1 FROM sqlite_master WHERE type = 'table'   AND name = 'quiz'   )
-    = 3
+    ( SELECT 1 FROM sqlite_master WHERE type = 'table'   AND name = 'season'             ) +
+    ( SELECT 1 FROM sqlite_master WHERE type = 'table'   AND name = 'meet'               ) +
+    ( SELECT 1 FROM sqlite_master WHERE type = 'table'   AND name = 'quiz'               ) +
+    ( SELECT 1 FROM sqlite_master WHERE type = 'trigger' AND name = 'meet_last_modified' ) +
+    ( SELECT 1 FROM sqlite_master WHERE type = 'trigger' AND name = 'quiz_last_modified' )
+    = 5
 THEN 1 ELSE 0 END;
--- config/db/app/1695323051_label/verify.sql
+++ config/db/app/1695323051_label/verify.sql
@@ -1,5 +1,7 @@
 SELECT CASE WHEN
-    ( SELECT 1 FROM sqlite_master WHERE type = 'table'   AND name = 'label'              ) +
-    ( SELECT 1 FROM sqlite_master WHERE type = 'trigger' AND name = 'label_after_update' )
-    = 2
+    ( SELECT 1 FROM sqlite_master WHERE type = 'table'   AND name = 'label'                  ) +
+    ( SELECT 1 FROM sqlite_master WHERE type = 'trigger' AND name = 'label_last_modified'    ) +
+    ( SELECT 1 FROM sqlite_master WHERE type = 'trigger' AND name = 'label_null_user_update' ) +
+    ( SELECT 1 FROM sqlite_master WHERE type = 'trigger' AND name = 'label_null_user_insert' )
+    = 4
 THEN 1 ELSE 0 END;
