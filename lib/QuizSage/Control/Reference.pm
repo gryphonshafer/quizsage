29f1ea39de3b96431d61242dea93d829894c341a
Gryphon Shafer <gryphon@goldenguru.com>
2024-06-14 14:26:21 -0700

    Reference tools scaffolding

---
+++ lib/QuizSage/Control/Reference.pm
@@ -0,0 +1,18 @@
+package QuizSage::Control::Reference;
+
+use exact 'Mojolicious::Controller';
+
+1;
+
+=head1 NAME
+
+QuizSage::Control::Reference
+
+=head1 DESCRIPTION
+
+This class is a subclass of L<Mojolicious::Controller> and provides handlers
+for "Reference" actions.
+
+=head1 INHERITANCE
+
+L<Mojolicious::Controller>.
