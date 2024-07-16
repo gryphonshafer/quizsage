ffb2eb472d4979310e7c5262c3a7143e63911437
Gryphon Shafer <gryphon@goldenguru.com>
2024-07-16 13:15:07 -0700

    Lookup implementation complete

--- static/js/vue/components/lookup/synonyms.js
+++ static/js/vue/components/lookup/synonyms.js
@@ -1,5 +1,33 @@
 import template from 'modules/template';
 
 export default {
+    data() {
+        return {
+            text     : '',
+            thesaurus: Object.keys( this.$root.material.data.thesaurus ).map( term => {
+                return {
+                    term    : term,
+                    text    : term.toLowerCase(),
+                    meanings: this.$root.material.data.thesaurus[term],
+                };
+            } ),
+        };
+    },
+
+    computed: {
+        content() {
+            const text = this.text
+                .toLowerCase()
+                .replace( /^\s+|\s+$/g, '' )
+                .replace( /[^\w\s]/g, '' );
+
+            const entry = this.thesaurus.find( item => item.text == text );
+
+            return ( entry && typeof entry.meanings === 'string' )
+                ? this.thesaurus.find( item => item.term == entry.meanings )
+                : entry;
+        },
+    },
+
     template: await template( import.meta.url ),
 };
--- static/js/vue/components/lookup/verses.js
+++ static/js/vue/components/lookup/verses.js
@@ -2,9 +2,10 @@ import template from 'modules/template';
 
 export default {
     data() {
+        const material   = this.$root.material;
         const references = [];
 
-        this.$root.material.data.ranges.forEach( range => {
+        material.data.ranges.forEach( range => {
             range.verses.forEach( reference => {
                 const found = reference.match(/^(?<book>.+)\s(?<chapter>\d+):(?<verse>\d+)$/);
                 let book    = references.find( element => element.book == found.groups.book );
@@ -29,15 +30,47 @@ export default {
         } );
 
         return {
-            material  : this.$root.material,
+            material  : material,
             references: references,
+            books     : references.map( element => element.book ),
             selected  : {
-                bible  : this.$root.material.bibles[0].name,
+                bible  : material.bibles[0].name,
                 book   : references[0].book,
                 chapter: Object.keys( references[0].chapters )[0],
                 verse  : references[0].chapters[ Object.keys( references[0].chapters )[0] ][0],
             },
         };
     },
+
+    computed: {
+        chapters() {
+            return Object.keys( this.references.find( book => book.book == this.selected.book ).chapters );
+        },
+        verses() {
+            return this.references
+                .find( book => book.book == this.selected.book )
+                .chapters[ this.selected.chapter ];
+        },
+        reference() {
+            return this.selected.book + ' ' + this.selected.chapter + ':' + this.selected.verse;
+        },
+        content() {
+            return this.material.lookup(
+                this.selected.bible,
+                this.selected.book,
+                this.selected.chapter,
+            );
+        },
+    },
+
+    watch: {
+        'selected.book': function () {
+            this.selected.chapter = this.chapters[0];
+        },
+        'selected.chapter': function () {
+            this.selected.verse = this.verses[0];
+        },
+    },
+
     template: await template( import.meta.url ),
 };
